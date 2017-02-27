ee484e231bf5e239e5cece3a94a53d15d8fa3df3
jyoung488 <jyoung488@gmail.com>
2017-02-27 10:46:56 -0800

    Add test for clicking new post link showing that page

--- spec/features/new_post_feature_spec_.rb
+++ spec/features/new_post_feature_spec_.rb
@@ -5,4 +5,10 @@ feature "create new post" do
     visit root_path
     expect(page).to have_link("New Post")
   end
+
+  scenario "the user sees form to create new post" do
+    visit root_path
+    click_link("New Post")
+    expect(current_path).to eq new_post_path
+  end
 end
