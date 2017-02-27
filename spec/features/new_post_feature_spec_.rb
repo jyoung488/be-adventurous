bd71468241e9b86fbc2e8c58bffafc92f7f247de
jyoung488 <jyoung488@gmail.com>
2017-02-27 10:52:02 -0800

    Add test for form on new post page

--- spec/features/new_post_feature_spec_.rb
+++ spec/features/new_post_feature_spec_.rb
@@ -6,9 +6,14 @@ feature "create new post" do
     expect(page).to have_link("New Post")
   end
 
-  scenario "the user sees form to create new post" do
+  scenario "the user can go to page for a new post" do
     visit root_path
     click_link("New Post")
     expect(current_path).to eq new_post_path
+  end
+
+  scenario "the user sees a form to create new post" do
+    visit new_post_path
+    expect(page)to have_button("Submit")
   end
 end
